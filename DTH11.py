dd9af5440d6098e461d01084a17c70647c229ff3
Yves Pattyn <YvesPattyn@gmail.com>
2020-01-05 16:55:12 +0000

    Initial work for DTH11 control

---
+++ DTH11.py
@@ -0,0 +1,11 @@
+class DTH11:
+  def __init__(self, pin, deviceType):
+    self.pin =pin
+    self.deviceType = deviceType
+    
+  def getTemp(self):
+    return 17.6
+
+  def getHumidity(self):
+    return 59.6
+
